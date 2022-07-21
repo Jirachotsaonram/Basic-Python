f412df8d580192c036cede20a7d29e460f9648f3
Jirachot Saontam <109726423+Jirachotsaonram@users.noreply.github.com>
2022-07-21 22:15:47 +0700

    3

---
+++ turtle nokey noscreenclick no.py
@@ -0,0 +1,42 @@
+import turtle
+import random
+
+
+t = turtle.Turtle()
+t.speed(0)
+t.width(5)
+colors =['red','blue']
+
+
+def up():
+    t.setheading(90)
+    t.forward(100)
+
+def down():
+    t.setheading(270)
+    t.forward(100)
+
+def left():
+    t.setheading(180)
+    t.forward(100) 
+
+def right():
+    t.setheading(0)
+    t.forward(100)
+
+def cilckleft(x,y):
+    t.color(random.choice(colors))
+
+def cilckright(x,y):
+    t.stamp()
+ 
+turtle.listen()
+
+turtle.onscreenclick(cilckleft, 1)
+turtle.onscreenclick(cilckright, 3)
+turtle.onkey(up, 'Up')
+turtle.onkey(down, 'Down')
+turtle.onkey(left, 'Left')
+turtle.onkey(right, 'Right')
+
+turtle.mainloop()
