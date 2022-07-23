bb7bbcffb18172d56439fcfb759fe70faca41c9b
Jirachot Saontam <109726423+Jirachotsaonram@users.noreply.github.com>
2022-07-23 23:17:49 +0700

    Update While Loop.py

--- While Loop.py
+++ While Loop.py
@@ -1,4 +1,9 @@
 i = 0
-while i < 100:
+while i < 1000:
     print(i)
-    i += 10
+    i += 10 #i += 10 เพิ่งครั้งละ10
+    if i == 30: #==เท่ากับ
+        break #หยุด
+        print('voi')
+        
+        
