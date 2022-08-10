2f5cdd783ca036a493bc049e0520af4423fa5eec
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2022-08-10 21:54:07 +0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+array1d = np.array([1,2,3,4,5,6,7,8,9,0])
+array2d = np.array([[1,3,4],[4,4,5],[7,7,5]])
+print(" =",array1d)
+print(" =",array2d)
+
+
+
+
+array1d = np.shape([4,3,4])
+array2d = np.shape([[5,4,2],[1,2,4],[9,8,7],[5,4,2]])
+print(" =",array1d)
+print(" =",array2d)
+
+
+
+array1d = np.size([1,3453,54,56,45])
+print(array1d)
+
+
+
+
+array1d = np.ndim([3,4,2,23,4,3,4,324,45,434,3])
+print(array1d)
+
+
+
+
+
+array2d = np.arange(12).reshape(3,4)
+array1d = np.arange(12)
+print(array1d)
+print(array2d)
