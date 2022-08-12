173e0750d05d3a03693580452e1bfb0045c7f680
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2022-08-12 21:06:38 +0700

    error

---
+++ 233.py
@@ -0,0 +1,23 @@
+n = int(input("enter number :"))
+male = 0
+
+for i in range(n):
+    male += 1
+    print("Hello",male)
+
+print("you")
+
+female = 0
+while female <= 10:
+    female += 1
+    print("go =",female)
+
+ho = 0
+for i in range(4):
+    ho += 1
+    print(ho)
+    return 1
+
+
+    
+        
